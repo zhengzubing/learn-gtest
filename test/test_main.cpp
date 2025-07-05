944226c71fb03bfe1ecf066784a22685fb319d76
zhengzubing <zizbee@163.com>
2025-07-06 01:50:31 +0800

    init envirment.

---
+++ test/test_main.cpp
@@ -0,0 +1,10 @@
+#include <gtest/gtest.h>
+
+TEST(TestDemo, EnvTest) {
+    EXPECT_EQ(2, 1 + 1);
+}
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
